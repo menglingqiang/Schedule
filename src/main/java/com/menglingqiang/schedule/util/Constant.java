e867a25cad9b510f84416f8a51a2aa92dc87978d
menglingqiang <1820026438@qq.com>
2018-03-02 14:37:15 +0800

    Update Constant.java

--- src/main/java/com/menglingqiang/schedule/util/Constant.java
+++ src/main/java/com/menglingqiang/schedule/util/Constant.java
@@ -5,15 +5,15 @@ public class Constant {
 	//本地地址
 	//public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
 	//public static final String USERPICLOCALPATH="/root/mlq/software/tomcat/tomcat8/apache-tomcat-8.5.24/webapps/schedule/WEB-INF/classes/static/userimg";
-	public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
+	public static final String USERPICLOCALPATH="yourServerUserImgLocal";
 	//服务器地址
-	public static final String SERVICELOCALPAHT="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg\\server";
+	public static final String SERVICELOCALPAHT="yourServerLocal";
 	//应用显示图标
 	public static final String WEBICO="panda.ico";
 	//发送用的邮箱
-	public static final String SENDEMAIL="1820026438@qq.com";
+	public static final String SENDEMAIL="yourEmail";
 	//发送用的邮箱授权码
-	public static final String SENDPASSWORD="upigsjirohopbfcd";
+	public static final String SENDPASSWORD="yourPoPCode";
 	//发送的标题
 	public static final String SENDTITLE="javaMail测试";
 	//邮箱注册用户
