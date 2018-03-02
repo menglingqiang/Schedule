5488458a61e6eb7d5e8565843e869f97a44cdc81
menglingqiang <1820026438@qq.com>
2018-03-02 22:23:21 +0800

    常量提交

---
+++ src/main/java/com/menglingqiang/schedule/util/Constant.java
@@ -0,0 +1,30 @@
+package com.menglingqiang.schedule.util;
+
+public class Constant {
+
+	//本地地址
+	//public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
+	//public static final String USERPICLOCALPATH="/root/mlq/software/tomcat/tomcat8/apache-tomcat-8.5.24/webapps/schedule/WEB-INF/classes/static/userimg";
+	public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
+	//服务器地址
+	public static final String SERVICELOCALPAHT="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg\\server";
+	//应用显示图标
+	public static final String WEBICO="panda.ico";
+	//发送用的邮箱
+	public static final String SENDEMAIL="1820026438@qq.com";
+	//发送用的邮箱授权码
+	public static final String SENDPASSWORD="upigsjirohopbfcd";
+	//发送的标题
+	public static final String SENDTITLE="天天打卡邮件提醒";
+	//邮箱注册用户
+	public static final String LOGINBYEMAIL="1";
+	//微博注册用户
+	public static final String LOGINBYWEIBO="2";
+	//微博本地调试
+	public static final String APPIDLOCAL="1179732476";
+	public static final String REURLLOCAL="http://apps.weibo.com/adfadfrtsh";
+	//微博网上调试
+	public static final String APPIDNET="3566366953";
+	public static final String REURLNET="http://www.menglingqiang.com/schedule/user/registerByThree?loginType=2";
+	
+}
