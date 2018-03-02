e8d3ff51ce917acaa374944425713297c7f1de65
menglingqiang <1820026438@qq.com>
2018-03-02 15:25:38 +0800

    常量替换

--- src/main/java/com/menglingqiang/schedule/util/Constant.java
+++ src/main/java/com/menglingqiang/schedule/util/Constant.java
@@ -5,26 +5,26 @@ public class Constant {
 	//本地地址
 	//public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
 	//public static final String USERPICLOCALPATH="/root/mlq/software/tomcat/tomcat8/apache-tomcat-8.5.24/webapps/schedule/WEB-INF/classes/static/userimg";
-	public static final String USERPICLOCALPATH="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg";
-	//服务器地址
-	public static final String SERVICELOCALPAHT="O:\\SpirngBoot\\schedule\\src\\main\\resources\\static\\userimg\\server";
+	//服务器地址用户头像存储地址
+	public static final String USERPICLOCALPATH="";
+	public static final String SERVICELOCALPAHT="";
 	//应用显示图标
 	public static final String WEBICO="panda.ico";
 	//发送用的邮箱
-	public static final String SENDEMAIL="1820026438@qq.com";
+	public static final String SENDEMAIL="";
 	//发送用的邮箱授权码
-	public static final String SENDPASSWORD="upigsjirohopbfcd";
+	public static final String SENDPASSWORD="";
 	//发送的标题
-	public static final String SENDTITLE="javaMail测试";
+	public static final String SENDTITLE="";
 	//邮箱注册用户
 	public static final String LOGINBYEMAIL="1";
 	//微博注册用户
 	public static final String LOGINBYWEIBO="2";
 	//微博本地调试
-	public static final String APPIDLOCAL="1179732476";
-	public static final String REURLLOCAL="http://apps.weibo.com/adfadfrtsh";
+	public static final String APPIDLOCAL="";
+	public static final String REURLLOCAL="";
 	//微博网上调试
-	public static final String APPIDNET="3566366953";
-	public static final String REURLNET="http://www.menglingqiang.com/schedule/user/registerByThree?loginType=2";
+	public static final String APPIDNET="";
+	public static final String REURLNET="";
 	
 }
